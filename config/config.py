609bea9a114dd66fd7689b885a7d5829b61e5283
MrNihalxx <119736729+MrNihalxx@users.noreply.github.com>
2022-12-05 00:49:42 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MrNihalxx/KiKaT",
+    "https://github.com/MrNihalxx/MrNihal",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
