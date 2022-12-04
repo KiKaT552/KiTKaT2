ae6c4c249ddcee98ea241fc009deabe1d658f08c
MrNihalxx <119736729+MrNihalxx@users.noreply.github.com>
2022-12-05 00:43:47 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH", "4c585632f990d84ac0bf7e881f49ebeb")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "5950085200:AAEhZunKQIArg1MXUW5QltFlfvH18cH5ONQ")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Music:Music@cluster0.f9x4i.mongodb.net/Cluster0?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://NihalMusic:Music@cluster0.ff1plbg.mongodb.net/?retryWrites=true&w=majority")
 
 DURATION_LIMIT_MIN = int(
     getenv("DURATION_LIMIT", "180")
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MrNihalXd/QueenMusic",
+    "https://github.com/MrNihalxx/KiKaT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
